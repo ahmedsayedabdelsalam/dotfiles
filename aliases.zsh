78372546e1b53ccea163da57b32ba63b44299f79
ahmed sayed abdelsalam <e.eng.ahmedsayed@gmail.com>
2022-02-01 10:40:16 +0200

    remove phpstorm

--- aliases.zsh
+++ aliases.zsh
@@ -2,7 +2,6 @@
 alias copyssh="pbcopy < $HOME/.ssh/id_ed25519.pub"
 alias reloadshell="source $HOME/.zshrc"
 alias ll="/opt/homebrew/opt/coreutils/libexec/gnubin/ls -AhlFo --color --group-directories-first"
-alias phpstorm='open -a /Applications/PhpStorm.app "`pwd`"'
 alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
 alias c="clear"
 
