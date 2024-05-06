775f1c8db0c82bea97b816781d806db1366869ab
Ahmed Sayed Abdelsalam <e.eng.ahmedsayed@gmail.com>
2024-05-06 19:03:34 +0300

    updates

--- nvchad/lua/plugins/treesitter.lua
+++ nvchad/lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
+    auto_install = true,
     ensure_installed = {
       -- defaults 
       "vim",
@@ -25,25 +26,39 @@ return {
       "cpp",
       "zig"
     },
-  },
-  dependencies = {
-    {
-      "JoosepAlviste/nvim-ts-context-commentstring",
-      opts = {
-        custom_calculation = function(_, language_tree)
-          if vim.bo.filetype == "blade" and language_tree._lang ~= "javascript" and language_tree._lang ~= "php" then
-            return "{{-- %s --}}"
-          end
-        end,
+    autotag = { -- 'windwp/nvim-ts-autotag'
+      enable = false, -- this breaks dot repeating with `>`
+    },
+    endwise = { -- 'RRethy/nvim-treesitter-endwise',
+      enable = true,
+    },
+    textobjects = { -- 'nvim-treesitter/nvim-treesitter-textobjects',
+      select = {
+        enable = true,
+        lookahead = true,
+        keymaps = {
+          ['if'] = '@function.inner',
+          ['af'] = '@function.outer',
+          ['ic'] = '@class.inner',
+          ['ac'] = '@class.outer',
+          ['il'] = '@loop.inner',
+          ['al'] = '@loop.outer',
+          ['ia'] = '@parameter.inner',
+          ['aa'] = '@parameter.outer',
+        },
       },
     },
+  },
+  dependencies = {
+    "windwp/nvim-ts-autotag",
+    "RRethy/nvim-treesitter-endwise",
     "nvim-treesitter/nvim-treesitter-textobjects",
+    "nvim-treesitter/nvim-treesitter-context",
   },
   config = function(_, opts)
     -- config override to support blade
     dofile(vim.g.base46_cache .. "syntax")
     dofile(vim.g.base46_cache .. "treesitter")
-    require("nvim-treesitter.configs").setup(opts)
 
     local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
     parser_config.blade = {
@@ -53,11 +68,11 @@ return {
           "src/parser.c",
           -- 'src/scanner.cc',
         },
-        branch = "main",
         generate_requires_npm = true,
         requires_generate_from_grammar = true,
       },
       filetype = "blade",
     }
+    require("nvim-treesitter.configs").setup(opts)
   end,
 }
