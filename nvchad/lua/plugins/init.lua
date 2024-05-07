1df110ce49ec7ee0d4f30b2e0791412dc83fe614
ahmed sayed abdelsalam <e.eng.ahmedsayed@gmail.com>
2024-05-07 16:31:43 +0300

    add cpp

--- nvchad/lua/plugins/init.lua
+++ nvchad/lua/plugins/init.lua
@@ -106,22 +106,23 @@ return {
   },
 
   -- run c++ code
-  {
-    "michaelb/sniprun",
-    branch = "master",
-
-    build = "sh install.sh",
-    -- do 'sh install.sh 1' if you want to force compile locally
-    -- (instead of fetching a binary from the github release). Requires Rust >= 1.65
-
-    config = function()
-      require("sniprun").setup({
-      -- your options
-      })
-    end,
-
-    keys = {
-      { "<leader>r", ":SnipRun<CR>", mode = "v" },
-    },
+  { -- This plugin
+    "Zeioth/compiler.nvim",
+    cmd = {"CompilerOpen", "CompilerToggleResults", "CompilerRedo"},
+    dependencies = { "stevearc/overseer.nvim" },
+    opts = {},
   },
+  { -- The task runner we use
+    "stevearc/overseer.nvim",
+    commit = "68a2d344cea4a2e11acfb5690dc8ecd1a1ec0ce0",
+    cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
+    opts = {
+      task_list = {
+        direction = "bottom",
+        min_height = 25,
+        max_height = 25,
+        default_detail = 1
+      },
+    },
+  }
 }
