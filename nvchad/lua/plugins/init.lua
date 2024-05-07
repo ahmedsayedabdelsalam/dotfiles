dc1c2887deefa675dce60fcebcb6b229ed1bb331
ahmed sayed abdelsalam <e.eng.ahmedsayed@gmail.com>
2024-05-07 18:42:26 +0300

    updates

--- nvchad/lua/plugins/init.lua
+++ nvchad/lua/plugins/init.lua
@@ -18,11 +18,7 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     opts = function()
-      return vim.tbl_deep_extend(
-        "force",
-        require("nvchad.configs.gitsigns"),
-        require("configs.gitsigns")
-      )
+      return vim.tbl_deep_extend("force", require "nvchad.configs.gitsigns", require "configs.gitsigns")
     end,
   },
 
@@ -33,25 +29,44 @@ return {
     },
   },
 
-  -- add live_grep_args
   {
-    "nvim-telescope/telescope.nvim",
-    dependencies = {
-      "nvim-telescope/telescope-live-grep-args.nvim",
-    },
+    "okuuva/auto-save.nvim",
+    cmd = "ASToggle", -- optional for lazy loading on command
+    event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
     opts = {
-      extensions = {
-        live_grep_args = {
-          auto_quoting = true,  -- random example from the README
-        },
-      },
-      extensions_list = {
-        "live_grep_args",
-        -- NvChad defaults
-        "themes",
-        "terms",
-      },
+      -- your config goes here
+      -- or just leave it empty :)
     },
+  },
+
+  {
+    "nvim-telescope/telescope-live-grep-args.nvim",
+    config = function()
+      require("telescope").setup {
+        extensions = {
+          live_grep_args = {
+            auto_quoting = true, -- random example from the README
+          },
+          extensions_list = {
+            "live_grep_args",
+          },
+        },
+      }
+    end,
+  },
+
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup {
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown {},
+          },
+        },
+      }
+      require("telescope").load_extension "ui-select"
+    end,
   },
 
   {
@@ -79,19 +94,19 @@ return {
   {
     "rcarriga/nvim-notify",
     config = function()
-        local notify = require("notify")
-        -- this for transparency
-        notify.setup({ background_colour = "#000000" })
-        -- this overwrites the vim notify function
-        vim.notify = notify.notify
-    end
+      local notify = require "notify"
+      -- this for transparency
+      notify.setup { background_colour = "#000000" }
+      -- this overwrites the vim notify function
+      vim.notify = notify.notify
+    end,
   },
 
   {
-    'akinsho/flutter-tools.nvim',
+    "akinsho/flutter-tools.nvim",
     dependencies = {
-        'nvim-lua/plenary.nvim',
-        'stevearc/dressing.nvim', -- optional for vim.ui.select
+      "nvim-lua/plenary.nvim",
+      "stevearc/dressing.nvim", -- optional for vim.ui.select
     },
     config = true,
   },
@@ -108,7 +123,7 @@ return {
   -- run c++ code
   { -- This plugin
     "Zeioth/compiler.nvim",
-    cmd = {"CompilerOpen", "CompilerToggleResults", "CompilerRedo"},
+    cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
     dependencies = { "stevearc/overseer.nvim" },
     opts = {},
   },
@@ -121,8 +136,8 @@ return {
         direction = "bottom",
         min_height = 25,
         max_height = 25,
-        default_detail = 1
+        default_detail = 1,
       },
     },
-  }
+  },
 }
