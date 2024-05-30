a191177f6e596d3c17e67924d30cb62c618a47bf
Ahmed Sayed Abdelsalam <e.eng.ahmedsayed@gmail.com>
2024-05-30 15:41:31 +0300

    fix treesitter

--- nvchad/lua/plugins/treesitter.lua
+++ nvchad/lua/plugins/treesitter.lua
@@ -1,91 +1,103 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    auto_install = true,
-    ensure_installed = {
-      -- defaults 
-      "vim",
-      "lua",
-      "vimdoc",
-      "bash",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      auto_install = true,
+      ensure_installed = {
+        -- defaults 
+        "vim",
+        "lua",
+        "vimdoc",
+        "bash",
 
-      -- web dev 
-      "html",
-      "css",
-      "javascript",
-      "typescript",
-      "tsx",
-      "vue",
-      "php",
-      "php_only",
-      "ruby",
+        -- web dev 
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "tsx",
+        "vue",
+        "php",
+        "php_only",
+        "ruby",
 
-     -- low level
-      "c",
-      "cpp",
-      "zig"
-    },
-    endwise = { -- 'RRethy/nvim-treesitter-endwise',
-      enable = true,
-    },
-    incremental_selection = {
-      enable = true,
-      keymaps = { -- set to `false` to disable one of the mappings
-        init_selection = "gnn",
-        node_incremental = "grn",
-        scope_incremental = "grc",
-        node_decremental = "grm",
+       -- low level
+        "c",
+        "cpp",
+        "zig"
       },
-    },
-    textobjects = { -- 'nvim-treesitter/nvim-treesitter-textobjects',
-      select = {
+      endwise = { -- 'RRethy/nvim-treesitter-endwise',
         enable = true,
-        lookahead = true,
-        keymaps = {
-          ['if'] = '@function.inner',
-          ['af'] = '@function.outer',
-          ['ic'] = '@class.inner',
-          ['ac'] = '@class.outer',
-          ['il'] = '@loop.inner',
-          ['al'] = '@loop.outer',
-          ['ia'] = '@parameter.inner',
-          ['aa'] = '@parameter.outer',
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = { -- set to `false` to disable one of the mappings
+          init_selection = "gnn",
+          node_incremental = "grn",
+          scope_incremental = "grc",
+          node_decremental = "grm",
+        },
+      },
+      textobjects = { -- 'nvim-treesitter/nvim-treesitter-textobjects',
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ['if'] = '@function.inner',
+            ['af'] = '@function.outer',
+            ['ic'] = '@class.inner',
+            ['ac'] = '@class.outer',
+            ['il'] = '@loop.inner',
+            ['al'] = '@loop.outer',
+            ['ia'] = '@parameter.inner',
+            ['aa'] = '@parameter.outer',
+          },
         },
       },
     },
-  },
-  dependencies = {
-    "windwp/nvim-ts-autotag",
-    "RRethy/nvim-treesitter-endwise",
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    "nvim-treesitter/nvim-treesitter-context",
-  },
-  config = function(_, opts)
-    -- config override to support blade
-    dofile(vim.g.base46_cache .. "syntax")
-    dofile(vim.g.base46_cache .. "treesitter")
+    dependencies = {
+      "RRethy/nvim-treesitter-endwise",
+      "nvim-treesitter/nvim-treesitter-textobjects",
+      "nvim-treesitter/nvim-treesitter-context",
+    },
+    config = function(_, opts)
+      -- config override to support blade
+      dofile(vim.g.base46_cache .. "syntax")
+      dofile(vim.g.base46_cache .. "treesitter")
 
-    local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-    parser_config.blade = {
-      install_info = {
-        url = "https://github.com/EmranMR/tree-sitter-blade",
-        files = {
-          "src/parser.c",
-          -- 'src/scanner.cc',
+      local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+      parser_config.blade = {
+        install_info = {
+          url = "https://github.com/EmranMR/tree-sitter-blade",
+          files = {
+            "src/parser.c",
+            -- 'src/scanner.cc',
+          },
+          branch = "main",
+          generate_requires_npm = true,
+          requires_generate_from_grammar = true,
         },
-        branch = "main",
-        generate_requires_npm = true,
-        requires_generate_from_grammar = true,
-      },
-      filetype = "blade",
-    }
+        filetype = "blade",
+      }
 
-    vim.filetype.add({
-      pattern = {
-        [".*%.blade%.php"] = "blade",
-      },
-    })
+      vim.filetype.add({
+        pattern = {
+          [".*%.blade%.php"] = "blade",
+        },
+      })
 
-    require("nvim-treesitter.configs").setup(opts)
-  end,
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    config = function ()
+      require('nvim-ts-autotag').setup({
+        filetypes = { "html" , "xml", "eruby", "embedded_template" }
+      })
+    end,
+    lazy = true,
+    event = "VeryLazy"
+  }
 }
