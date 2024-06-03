c96f95cf04f9f07e1c579a08f6b52c3848c70c83
Ahmed Sayed Abdelsalam <e.eng.ahmedsayed@gmail.com>
2024-06-04 01:42:56 +0300

    add html & css

--- lazyvim/lua/plugins/mason.lua
+++ lazyvim/lua/plugins/mason.lua
@@ -2,6 +2,10 @@ return {
   "williamboman/mason.nvim",
   opts = function(_, opts)
     vim.list_extend(opts.ensure_installed, {
+      -- web dev stuff
+      "html-lsp",
+      "emmet-ls",
+      "css-lsp",
 
       -- php stuff
       "intelephense",
