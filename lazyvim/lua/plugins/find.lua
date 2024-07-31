025c60afba852b8ca88fbe11454e3a885ab97cb3
Ahmed Sayed Abdelsalam <e.eng.ahmedsayed@gmail.com>
2024-08-01 00:01:53 +0300

    add find in dirs

---
+++ lazyvim/lua/plugins/find.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "princejoogie/dir-telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+    },
+    keys = {
+      {
+        "<leader>sG",
+        "<cmd>Telescope dir live_grep<CR>",
+        noremap = true,
+        silent = true,
+        desc = "Find in dir",
+      },
+    },
+  },
+}
