e34205a77c49d3cbda24b8605c6286c00ca6be09
Ahmed Sayed Abdelsalam <e.eng.ahmedsayed@gmail.com>
2024-12-16 21:35:31 +0200

    updates

--- lazyvim/lua/plugins/test.lua
+++ lazyvim/lua/plugins/test.lua
@@ -1,11 +1,26 @@
 return {
-  "nvim-neotest/neotest",
-  dependencies = {
-    "zidhuss/neotest-minitest",
+  {
+    "rcasia/neotest-java",
+    ft = "java",
+    dependencies = {
+      "mfussenegger/nvim-jdtls",
+      "mfussenegger/nvim-dap", -- for the debugger
+      "rcarriga/nvim-dap-ui", -- recommended
+      "theHamsta/nvim-dap-virtual-text", -- recommended
+    },
   },
-  opts = {
-    adapters = {
-      "neotest-minitest",
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "zidhuss/neotest-minitest",
+    },
+    opts = {
+      adapters = {
+        "neotest-minitest",
+        ["neotest-java"] = {
+          -- config here
+        },
+      },
     },
   },
 }
