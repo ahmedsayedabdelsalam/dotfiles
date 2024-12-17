5d50033cd671d9e1633b09fce3c4de643d817e15
Ahmed Sayed Abdelsalam <e.eng.ahmedsayed@gmail.com>
2024-12-17 11:11:43 +0200

    updates

--- fresh.sh
+++ fresh.sh
@@ -55,7 +55,7 @@ ln -s karabiner.json $HOME/.config/karabiner/karabiner.json
 
 # Removes .ideavimrc from $HOME/.ideavimrc (if it exists) and symlinks the .ideavimrc file from the .dotfiles
 rm -rf $HOME/.ideavimrc
-ln -s .ideavimrc $HOME/.ideavimrc
+ln -s $DOTFILES/.ideavimrc $HOME/.ideavimrc
 
 # Set iterm config files.
 defaults write com.googlecode.iterm2 PrefsCustomFolder -string "$DOTFILES/iterm2"
