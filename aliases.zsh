a601c4ee3a90f100de5b821679904b7aacc56f7d
Ahmed Sayed Abdelsalam <e.eng.ahmedsayed@gmail.com>
2026-05-05 17:48:08 +0300

    updates

--- aliases.zsh
+++ aliases.zsh
@@ -10,7 +10,6 @@ alias vi=nvim
 alias k=kubectl
 alias tm='tmux attach -t default || tmux new -s default'
 alias avante='nvim -c "lua vim.defer_fn(function()require(\"avante.api\").zen_mode()end, 100)"'
-alias restart-aerospace-swipe="launchctl bootout gui/$(id -u) ~/Library/LaunchAgents/com.acsandmann.swipe.plist 2>/dev/null && launchctl bootstrap gui/$(id -u) ~/Library/LaunchAgents/com.acsandmann.swipe.plist"
 alias oc=opencode
 alias lg=lazygit
 alias ghd="gh dash"
@@ -19,7 +18,6 @@ alias ghd="gh dash"
 alias fixmounthdd='sudo pkill -f fsck'
 alias fixturnoffspotlight='sudo mdutil -i off'
 alias fixturnonspotlight='sudo mdutil -i on'
-alias fixaerospaceswipe="launchctl bootout gui/$(id -u) ~/Library/LaunchAgents/com.acsandmann.swipe.plist 2>/dev/null && launchctl bootstrap gui/$(id -u) ~/Library/LaunchAgents/com.acsandmann.swipe.plist"
 alias fixpostgres="rm -rf /opt/homebrew/var/postgresql@18/postmaster.pid && brew services restart postgresql"
 
 # Eza (better ls)
@@ -65,6 +63,58 @@ alias gbc="$DOTFILES/git-clean.sh"
 alias gcz="git cz"
 alias glm="submodule update --remote --merge"
 alias gpm="git push --recurse-submodules=on-demand"
+
+
+#################################################################################
+#################################################################################
+#################################################################################
+# https://github.com/ohmyzsh/ohmyzsh/blob/master/lib/directories.zsh
+# Changing/making/removing directory
+setopt auto_cd
+setopt auto_pushd
+setopt pushd_ignore_dups
+setopt pushdminus
+
+
+alias -g ...='../..'
+alias -g ....='../../..'
+alias -g .....='../../../..'
+alias -g ......='../../../../..'
+
+alias -- -='cd -'
+alias 1='cd -1'
+alias 2='cd -2'
+alias 3='cd -3'
+alias 4='cd -4'
+alias 5='cd -5'
+alias 6='cd -6'
+alias 7='cd -7'
+alias 8='cd -8'
+alias 9='cd -9'
+
+alias md='mkdir -p'
+alias rd=rmdir
+
+function d () {
+  if [[ -n $1 ]]; then
+    dirs "$@"
+  else
+    dirs -v | head -n 10
+  fi
+}
+compdef _dirs d
+
+# List directory contents
+alias lsa='ls -lah'
+alias l='ls -lah'
+alias ll='ls -lh'
+alias la='ls -lAh'
+
+#################################################################################
+#################################################################################
+#################################################################################
+
+
 
 #################################################################################
 #################################################################################
